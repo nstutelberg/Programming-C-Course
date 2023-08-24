e1d32ea6ea429a44b05b1f49544b1859679dcbb8
Nolan Stutelberg <nstutelberg@gmail.com>
2023-08-23 21:40:14 -0500

    add test files

---
+++ practice_files/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+
+// Note that if you want to add a new line you can do it within the string with `\n`
+{
+    int suppliedNum = 5;
+    printf("Hello, world!\nAre you good today?\n");
+    printf("Hello my number is %d", suppliedNum);
+}
