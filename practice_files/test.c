9a3b9809845f647bc7e3432a7b633fc7806c0e33
Nolan Stutelberg <nstutelberg@gmail.com>
2023-08-24 20:35:23 -0500

    update week 1 files

--- practice_files/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-
-// Note that if you want to add a new line you can do it within the string with `\n`
-{
-    int suppliedNum = 5;
-    printf("Hello, world!\nAre you good today?\n");
-    printf("Hello my number is %d", suppliedNum);
-}
