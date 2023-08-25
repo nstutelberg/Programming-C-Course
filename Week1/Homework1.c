3c6de5d8b558d768c2b4fd55d55304cb66b8ffbe
Nolan Stutelberg <nstutelberg@gmail.com>
2023-08-25 00:12:36 -0500

    update homework file

--- Week1/Homework1.c
+++ Week1/Homework1.c
@@ -3,17 +3,18 @@
 
 int main()
 {
-    // Specify the input types that the user will enter
     char inputCharacter;
     int inputInteger;
     float inputFloat;
 
-    printf("HW #01, Nolan Stutelberg\n");
+    printf("HW #01, Nolan Stutelberg\n\n");
     printf("Enter your input in this order with a space between each value: character integer floating-point-number: ");
 
     // Accept input, but use a pointer because C needs to be able to modify the original variable value that is stored in memory in order for the `printf` statements below to return the stored value
+    // We are not using a pointer with a string or string array so this is acceptable (I hope)
     scanf("%c %d %f", &inputCharacter, &inputInteger, &inputFloat);
 
+    // Limiting the length of the character to 1
     printf("%1c\n", inputCharacter);
     printf("%d\n", inputCharacter);
     printf("%d\n", inputInteger);
