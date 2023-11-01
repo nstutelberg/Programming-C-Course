66ee4b08e594137737ad8de8672378a7d1507827
Nolan Stutelberg <nstutelberg@gmail.com>
2023-10-31 22:23:09 -0500

    finish homework

--- Week11/Homework11.c
+++ Week11/Homework11.c
@@ -10,13 +10,15 @@ void clearInputBuffer();
 int main()
 {
     printf("HW #11, Nolan Stutelberg\n\n");
-    char userInput[SIZE] = "";
-    int matches[MAXMATCHES] = {0};
+    char userInput[SIZE];
+    int matches[MAXMATCHES];
 
     while (userInput[0] != '\n')
     {
         printf("Enter a string to search for matches, or enter a blank line to exit the program -> ");
         fgets(userInput, SIZE, stdin);
+
+        // Getting the input length to reuse in various calculations, as to not get outOfBounds errors and clear input buffers
         int inputLength = strlen(userInput);
 
         if (userInput[inputLength - 1] != '\n')
@@ -33,22 +35,21 @@ int main()
                 printf("Input truncated to %d characters -> %s\n\n", SIZE - 1, userInput);
             }
 
+            // Below is setting a temporary buffer for teh user input and setting it to 3, because 1 is for a character the user enters, one is for the end of the string (null terminator), and one is for the end of the string
+            // This section of the code makes sure that the first character the user enters is used.. So you can enter `aousdhfiauoshf` as the targetChar, and only `a` will actually be used
             char targetChar;
-
-            // for char, newline, and '\0'
             char tempBuffer[3];
             printf("Enter character to search for -> ");
             fgets(tempBuffer, 3, stdin);
             targetChar = tempBuffer[0];
 
-            // Clear input buffer again in case the character input also exceeds the buffer
+            // Clear input buffer again in case the character input exceeds 1 character. This is so the next steps won't use leftover characters that are being stored in the buffer
             if (tempBuffer[strlen(tempBuffer) - 1] != '\n')
             {
                 clearInputBuffer();
             }
 
             int count = charIsAt(userInput, targetChar, matches, MAXMATCHES);
-
             printf("\nThe character '%c' was found %d time(s) in the word you inputted.", targetChar, count);
 
             // Only execute the rest of the code if there were any matches at all. Or else just return 0 and exit the program
@@ -67,7 +68,8 @@ int main()
                     printf("\n");
                 }
 
-                // Print the up arrows under the numbers.
+                // Print the up arrows only at the positions where there were matches. the charIsAt function populates the matches array with indices for all the matched characters, so that array is used to tell
+                // the program when to print out the '^' and when to print a ' '
                 for (int i = 0, j = 0; userInput[i] != '\0'; i++)
                 {
                     if (j < count && i == matches[j])
@@ -113,3 +115,30 @@ void clearInputBuffer()
     while ((c = getchar()) != '\n' && c != EOF)
         ;
 }
+
+/*
+Sample output:
+
+HW #11, Nolan Stutelberg
+
+Enter a string to search for matches, or enter a blank line to exit the program -> hello prof pottebaum whats good
+Enter character to search for -> p
+
+The character 'p' was found 2 time(s) in the word you inputted.
+The 2 matches are at the following index/indices-> 6 11 and the positions in the string where matches were found are shown below
+hello prof pottebaum whats good
+      ^    ^
+
+Enter a string to search for matches, or enter a blank line to exit the program -> thisisareallylongstringthatwillbetruncatedweeeeeeeeeeeeeeeeeeeeeeeeeee
+Input truncated to 40 characters -> thisisareallylongstringthatwillbetruncat
+
+Enter character to search for -> l
+
+The character 'l' was found 5 time(s) in the word you inputted.
+The 5 matches are at the following index/indices-> 10 11 13 29 30 and the positions in the string where matches were found are shown below
+thisisareallylongstringthatwillbetruncat
+          ^^ ^               ^^
+
+Enter a string to search for matches, or enter a blank line to exit the program ->
+PS C:\Users\nstut\git\Programming-C-Course\Week11>
+*/
